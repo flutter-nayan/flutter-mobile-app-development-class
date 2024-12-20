e3525d3bbe6f11bdd3ab5806523dafc10bce5fe0
Mohammad Nayan <flutter.nayan@gmail.com>
2024-12-20 19:37:43 +0600

    Class 12: Contact form validation, screen to screen navigation

--- lib/pages/FormValidation.dart
+++ lib/pages/FormValidation.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/Counter.dart';
 
 class FormValidation extends StatelessWidget {
+  GlobalKey<FormState> _formkey = GlobalKey();
     
    FormValidation({super.key});
-  _submitForm(){
+ void _submitForm(){
+  if(_formkey.currentState!.validate()){
+
+  }
 
   }
   @override
@@ -11,14 +16,26 @@ class FormValidation extends StatelessWidget {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(8.0),
-        child: Form(child: 
+        child: Form(
+          key: _formkey,
+          child: 
         
         Column(
           children: [
+            TextButton(onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>CustomCounter()));
+            }, child: Text("Click")),
             Text("Contact Form",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,
             fontSize: 40.0),),
             SizedBox(height: 20.0,),
             TextFormField(
+              validator: (value){
+                if(value!.isEmpty){
+                  return "User name is required";
+                }else{
+                  return null;
+                }
+              },
               decoration: InputDecoration(
                 labelText: "User name",
                 border: OutlineInputBorder(
@@ -29,7 +46,13 @@ class FormValidation extends StatelessWidget {
             ),
             SizedBox(height: 5.0,),
             TextFormField(
-              
+              validator:  (value){
+                if(value!.isEmpty){
+                  return "Email is required";
+                }else{
+                  return null;
+                }
+              },
                decoration: InputDecoration(
                 labelText: "Email",
                 border: OutlineInputBorder(
@@ -39,6 +62,13 @@ class FormValidation extends StatelessWidget {
             ),
              SizedBox(height: 5.0,),
             TextFormField(
+              validator:  (value){
+                if(value!.isEmpty){
+                  return "Phone no is required";
+                }else{
+                  return null;
+                }
+              },
                decoration: InputDecoration(
                 labelText: "Phone",
                 border: OutlineInputBorder(
@@ -51,6 +81,8 @@ class FormValidation extends StatelessWidget {
               height: 50.0,
               width: double.infinity,
               child: ElevatedButton(onPressed: (){
+                  _submitForm();
+                  print(_formkey.currentState);
 
               }, child: Text("Submit Button",style: TextStyle(color: Colors.white),),
               style: ElevatedButton.styleFrom(
